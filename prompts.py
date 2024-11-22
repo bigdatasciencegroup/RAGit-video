70c1edfc6e6fe4618c3695aaf9946035a90773af
L David <23113728+raineydavid@users.noreply.github.com>
2024-11-22 20:19:29 +0000

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -1,18 +1,18 @@
 from langchain.prompts import PromptTemplate
 
 question_prompt_template = """ 
-Eres una inteligencia artificial diseñada para responder preguntas sobre el contenido que aparece en un video.
- Esta es una transcripción del video:
+You are an artificial intelligence designed to answer questions about the content of a video.
+This is a transcript of the video:
 ---
-TRANSCRIPCIÓN: {context}
+TRANSCRIPT: {context}
 --- 
 
-Utilizando el contenido de la transcripción mencionada arriba, responde la siguiente pregunta.
-Si la pregunta es compleja respondela por partes:
+Using the content of the transcript above, answer the following question.
+If the question is complex, answer it step by step:
 
-PREGUNTA: {question}
+QUESTION: {question}
 
-REPSUESTA:
+ANSWER:
 """
 
 question_prompt = PromptTemplate(
@@ -22,49 +22,53 @@ question_prompt = PromptTemplate(
 
 
 fill_punctuation_template = """
-Eres un experto trascribiendo videos. Tu tarea consiste en editar trasncripciones de video sin puntuación ni mayúsculas 
-y devolver la misma transcripción con puntuación, comas y mayúsculas correctas.
+You are an expert in transcribing videos. Your task is to edit video transcriptions that lack punctuation or capitalization 
+and return the same transcription with correct punctuation, commas, and capitalization.
 
-Ejemplo:
+Example:
 
-Entrada:
+Input:
 
-el dia de hoy vamos a hablar sobre la importancia de la puntuacion en la escritura
+today we are going to talk about the importance of punctuation in writing
 
-Salida:
+Output:
 
-El día de hoy vamos a hablar sobre la importancia de la puntuación en la escritura.
+Today, we are going to talk about the importance of punctuation in writing.
 
-Instrucciones:
+Instructions:
 
-Analiza la entrada:
+Analyze the input:
 
-Identifica las palabras, frases y oraciones dentro de la transcripción.
-Reconoce los diferentes tipos de palabras (sustantivos, verbos, adjetivos, etc.).
-Detecta la estructura de las oraciones (sujeto, verbo, objeto).
-Predice la puntuación:
+- Identify the words, phrases, and sentences in the transcription.
+- Recognize the different types of words (nouns, verbs, adjectives, etc.).
+- Detect the sentence structure (subject, verb, object).
 
-Coma (,): Separa elementos dentro de una frase.
-Punto (.): Indica el final de una oración.
-Punto y coma (;): Separa dos oraciones independientes relacionadas entre sí.
-Puntos suspensivos (...): Indican que la oración está incompleta o que se omite información.
-Signos de interrogación (?): Indican una pregunta.
-Signos de exclamación (!): Indican sorpresa, énfasis o emoción.
-Predice las mayúsculas:
+Predict punctuation:
 
-Primera letra de la oración: Debe ser mayúscula.
-Nombres propios: Deben comenzar con mayúscula.
-Títulos y cargos: Deben comenzar con mayúscula.
-Considera el contexto:
+- Comma (,): Separates elements within a sentence.
+- Period (.): Indicates the end of a sentence.
+- Semicolon (;): Separates two independent but related sentences.
+- Ellipsis (...): Indicates that the sentence is incomplete or information is omitted.
+- Question mark (?): Indicates a question.
+- Exclamation mark (!): Indicates surprise, emphasis, or emotion.
 
-El estilo de puntuación puede variar según el contexto y el tipo de texto.
-Es importante ser consistente con la puntuación a lo largo de la transcripción.
-La puntuación correcta puede mejorar la legibilidad y la comprensión del texto.
+Predict capitalization:
 
-Entrada: {raw_transcription}
+- First letter of the sentence: Must be capitalized.
+- Proper nouns: Must start with a capital letter.
+- Titles and roles: Must start with a capital letter.
 
-Salida:
+Consider the context:
+
+- The style of punctuation may vary depending on the context and type of text.
+- It is important to be consistent with punctuation throughout the transcription.
+- Correct punctuation can improve readability and comprehension.
+
+Input: {raw_transcription}
+
+Output:
 """
+
 fill_punctuation_prompt = PromptTemplate(
     template=fill_punctuation_template,
     input_variables=["raw_transcription"]
